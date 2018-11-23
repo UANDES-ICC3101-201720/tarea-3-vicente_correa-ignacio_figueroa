07a3b3314846d9f3943994c8b86e9665cf7d563a
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2018-11-23 18:09:55 -0400

    Server

--- cliente.py
+++ cliente.py
@@ -1,9 +1,42 @@
-import socket               # Import socket module
+import socket
+import sys
+from os import scandir, getcwd
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+def ls(ruta = getcwd()):
+    return [arch.name for arch in scandir(ruta) if arch.is_file()]
+host = '192.168.0.18'
+print(host)
+port = 50007
+BUFFER_SIZE = 1024
+file = "recibido2.jpg"
 
-s.connect((host, port))
-print (s.recv(1024))
-s.close()
+archivos = []
+for arc in ls():
+    if ".txt" in arc or ".jpg" in arc:
+        archivos.append(arc)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("socket creado")
+    s.connect((host, port))
+    print("conectado")
+    mensaje = "<{}> <{}> HELLO <{}>"
+    s.send(mensaje.format(port, host, socket.gethostname()))
+    s.sendto(mensaje.format(port, host, socket.gethostname()), (host, port))
+
+    print("Que deseas hacer?\n"
+          "\t (1) Buscar archivo\n"
+          "\t (2) Enviar archivo\n"
+          "\t (3) Salir\n")
+    opcion = input("Ingrese su opcion: ")
+    if opcion == "1":
+        pass
+    elif opcion == "2":
+        pass
+    elif opcion == "3":
+        sys.exit(0)
+
+    with open(file, "wb") as f:
+        BUFFER = s.recv(BUFFER_SIZE)
+        while BUFFER:
+            f.write(BUFFER)
+            BUFFER = s.recv(BUFFER_SIZE)
--- server.py
+++ server.py
@@ -1,13 +1,71 @@
 import socket  # Import socket module
 
-s = socket.socket()  # Create a socket object
-host = socket.gethostname()  # Get local machine name
-port = 12345  # Reserve a port for your service.
-s.bind((host, port))  # Bind to the port
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 50007
+BUFFER_SIZE = 1024
+FILE = "example.txt"
+online = []
+class client:
+    def __init__(self):
+        self.ip = ""
+        self.conn = ""
+        self.port = ""
+        self.files = []
 
-s.listen(5)  # Now wait for client connection.
-while True:
-    c, addr = s.accept()  # Establish connection with client.
-    print ('Got connection from', addr)
-    c.send('Thank you for connecting')
-    c.close()
+clientePrueba = client()
+clientePrueba.ip = "192.168.0.31"
+clientePrueba.port = "45678"
+clientePrueba.files=["Foto1.jpg", "Foto2.jpg", "Prueba.txt"]
+sockets = []
+with socket.socket() as s:
+    online.append(clientePrueba)
+
+    s.bind((HOST, PORT))
+    s.listen(5)
+    cliente = client()
+
+    cliente.conn, addr = s.accept()
+
+    received = cliente.conn.recv(BUFFER_SIZE).decode()
+    received = received.split(" ")
+    cliente.port = received[0][1:-1]
+    print (received)
+    cliente.ip = received[1][1:-1]
+    online.append(cliente)
+    message = "<{}> HOW ARE U, GIVE ME YOUR FILENAMES <{}>".format(PORT, cliente.ip)
+    sent = cliente.conn.send(message.encode())
+    file = cliente.conn.recv(BUFFER_SIZE)
+    while file:
+        cliente.files.append(file.decode())
+        sent = cliente.conn.send("OK".encode())
+        file = cliente.conn.recv(BUFFER_SIZE)
+        print(file.decode())
+        if file.decode() == "FINISH":
+            break
+    while True:
+        received = cliente.conn.recv(BUFFER_SIZE).decode()
+        if "IM SEARCHING" in received:
+            cliente.conn.send("OK ILL SEARCH".encode())
+            file = received.split(" ")[2]
+            encountered = False
+            for c in online:
+                if file in c.files:
+                    cliente.conn.send("{} HAS IT".format(c.ip).encode())
+                    encountered = True
+                    response = cliente.conn.recv(BUFFER_SIZE).decode()
+                    if response == "OK, TELL ME MORE":
+                        continue
+                    else:
+                        print("error")
+                        break
+
+            if not encountered:
+                cliente.conn.send("NO ONE HAS IT :(".encode())
+            else:
+                cliente.conn.send("THATS ALL FOLK".encode())
+        if "I DONT NEED U ANYMORE" in received:
+            online.remove(cliente)
+            cliente.conn.send("OK, BAI {}".format(cliente.ip).encode())
+            s.close()
+
+
